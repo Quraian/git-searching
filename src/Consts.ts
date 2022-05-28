3e8503dd98835026cee018e1838852f113e6161f
Mohammad AlQuraian <m4design@gmail.com>
2022-05-28 22:59:32 +0300

    Updates

--- src/Consts.ts
+++
@@ -1,7 +0,0 @@
-export class Colors {
-  static readonly DARK_GRAY = "rgb(87, 96, 106)";
-}
-
-export class Defaults {
-  static readonly PER_PAGE = 5;
-}
--- src/models/user.model.ts
+++
@@ -1,8 +0,0 @@
-export interface User {
-  id: number;
-  name: string;
-  login: string;
-  bio: string;
-  avatar_url: string;
-  html_url: string;
-}
